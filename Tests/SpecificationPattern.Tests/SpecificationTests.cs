2604e96a410b3595be422a80e5876242c3da0944
Sy <greenmeany.sg@googlemail.com>
2015-02-19 19:51:33 +0000

    Created Specification class
    Created specification class and matching test class

---
+++ Tests/SpecificationPattern.Tests/SpecificationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpecificationPattern.Tests
+{
+    /// <summary>
+    /// Unit tests for <see cref="Specification"/>
+    /// </summary>
+    [TestClass]
+    public class SpecificationTests
+    {
+       
+    }
+}
---
+++ src/SpecificationPattern/Specification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificationPattern
+{
+    /// <summary>
+    /// Determines if an object meets a specification
+    /// </summary>
+    public class Specification
+    {
+    }
+}
