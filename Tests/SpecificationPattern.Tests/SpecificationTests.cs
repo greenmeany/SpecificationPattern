c1da2973beeffa484c28d3a05fe0790035f4119d
Sy <greenmeany.sg@googlemail.com>
2015-02-19 21:42:16 +0000

    Implemented and tested OR and AND functionality of specification
    Implemented functionality for OR and AND. Added nuget restore workflow
    on solution

--- Tests/SpecificationPattern.Tests/SpecificationTests.cs
+++ Tests/SpecificationPattern.Tests/SpecificationTests.cs
@@ -9,6 +9,52 @@ namespace SpecificationPattern.Tests
     [TestClass]
     public class SpecificationTests
     {
-       
+        /// <summary>
+        /// Ensure simple usage of the scenario can be met easily
+        /// </summary>
+        [TestMethod]
+        public void SimpleUsageScenario_Test()
+        {
+            var lessThanTenSpecifcation = new Specification<int>(i=>i<=10);
+            Assert.IsTrue(lessThanTenSpecifcation.IsSatisfiedBy(9));
+        }
+
+        /// <summary>
+        /// Simple test to ensure And functionality for Specification works
+        /// </summary>
+        [TestMethod]
+        public void SimpleAndUsageScenario_Test()
+        {
+            var stringLengthTen =  new Specification<string>(s => s.Length == 10);
+            var stringLengthTenAndBeginsWithS = stringLengthTen.And(s => s[0] == 'S');
+
+            var testStringOne = "SteakTasty";
+            var testStringTwo = "CigsRNasty";
+
+            Assert.IsTrue(stringLengthTenAndBeginsWithS.IsSatisfiedBy(testStringOne));
+            Assert.IsFalse(stringLengthTenAndBeginsWithS.IsSatisfiedBy(testStringTwo));
+        }
+
+        /// <summary>
+        /// Simple test to ensure Or functionality for Specification works
+        /// </summary>
+        [TestMethod]
+        public void SimpleOrUsageScenario_Test()
+        {
+            var stringLengthTen = new Specification<string>(s => s.Length == 10);
+            var stringLengthTenOrBeginsWithS = stringLengthTen.Or(s => s[0] == 'S');
+
+            var testStringOne = "SteakTasty";
+            var testStringTwo = "CigsRNasty";
+            var testStringThree = "S";
+            var testStringFour = "C";
+
+            Assert.IsTrue(stringLengthTenOrBeginsWithS.IsSatisfiedBy(testStringOne));
+            Assert.IsTrue(stringLengthTenOrBeginsWithS.IsSatisfiedBy(testStringTwo));
+            Assert.IsTrue(stringLengthTenOrBeginsWithS.IsSatisfiedBy(testStringThree));
+            Assert.IsFalse(stringLengthTenOrBeginsWithS.IsSatisfiedBy(testStringFour));
+        }
+
+
     }
 }
--- src/SpecificationPattern/Specification.cs
+++ src/SpecificationPattern/Specification.cs
@@ -1,15 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using LinqKit;
 
 namespace SpecificationPattern
 {
     /// <summary>
     /// Determines if an object meets a specification
     /// </summary>
-    public class Specification
+    public class Specification<T>
     {
+
+        /// <summary>
+        /// Predicate that checks an object satisfies a given specification
+        /// </summary>
+        public Expression<Func<T, bool>> Predicate { get; private set; }
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="specification">Specification as a bool</param>
+        public Specification(Expression<Func<T,bool>> specification)
+        {
+            Predicate = specification;
+        }
+
+        /// <summary>
+        /// New specifiaction where candidates must satisfy this AND the new specification
+        /// </summary>
+        /// <param name="other">Other Specification</param>
+        /// <returns>New specification</returns>
+        public Specification<T> And(Expression<Func<T,bool>> other)
+        {          
+            return new Specification<T>(Predicate.And(other));
+        }
+
+        /// <summary>
+        /// Builds new specification where candidates must satisfy this OR the new specification
+        /// </summary>
+        /// <param name="other">Other Specification</param>
+        /// <returns>New specification</returns>
+        public Specification<T> Or(Expression<Func<T, bool>> other)
+        {
+            return new Specification<T>(Predicate.Or(other));
+        }
+
+        /// <summary>
+        /// Determines if candidate meets the specified criteria
+        /// </summary>
+        /// <param name="candidate">Candidate to evaluate</param>
+        /// <returns></returns>
+        public bool IsSatisfiedBy(T candidate)
+        {
+            return Predicate.Compile().Invoke(candidate);
+        }
+
+
     }
 }
